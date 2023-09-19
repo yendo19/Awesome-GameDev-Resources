4639d99801997e8ef37506bfa61bf75aed78474e
yendo19 <90066698+yendo19@users.noreply.github.com>
2023-09-19 18:10:58 -0400

    confused

--- courses/artificialintelligence/assignments/life/life.cpp
+++ courses/artificialintelligence/assignments/life/life.cpp
@@ -2,61 +2,78 @@
 using namespace std;
 int main(){
     int C, L, T;
-
+    int counter;
    cin >> C >> L >> T;
    char grid[C][L];
+   char temp[C][L];
    for(int i = 0; i<C; i++)
    {
      for(int k = 0; k<L; k++)
      {
        cin >> grid[i][k];
+       temp[i][k] = '.';
      }
    }
-
-   for(int j = 0; j<T; j++)
-   {
-   for(int i = 0; i<C; i++) {
+   for(int j = 0; j<T; j++) {
+     for (int i = 0; i < C; i++) {
        for (int k = 0; k < L; k++) {
-         if (grid[i][k] == '#') {
-           int counter = 0;
-           if (grid[i + 1][k] != NULL)
-             if (grid[i + 1][k] == '#') counter++;
-           if (grid[i - 1][k] != NULL)
-             if (grid[i - 1][k] == '#') counter++;
-           if (grid[i][k + 1] != NULL)
-             if (grid[i][k + 1] == '#') counter++;
-           if (grid[i][k - 1] != NULL)
-             if (grid[i][k - 1] == '#') counter++;
 
-           switch (counter) {
-             case 0:
-               grid[i][k] = '.';
-               break;
-             case 1:
-               grid[i][k] = '.';
-               break;
-             case 2:
-               grid[i][k] = '.';
-             case 4:
-               grid[i][k] = '.';
-               break;
-             default:
-               break;
+         counter = 0;
+         if(i==C)
+         {
+           if (grid[0][k] == '#') counter++;
+         }
+         else
+             if (grid[i + 1][k] == '#') counter++;
+         if(i == 0)
+         {
+           if (grid[C][k] == '#') counter++;
+         }
+         else
+             if (grid[i - 1][k] == '#') counter++;
+
+         if(k==L)
+         {
+           if (grid[i][0] == '#') counter++;
+         }
+         else
+             if (grid[i][k+1] == '#') counter++;
+         if(k == 0)
+         {
+           if (grid[i][L] == '#') counter++;
+         }
+         else
+             if (grid[i][k-1] == '#') counter++;
+         if(C != 3)
+         if (grid[i][k] == '#')
+         {
+           if(counter < 2)
+           {
+             temp[i][k] = '.';
            }
-         } else {
-           int counter = 0;
-           if (grid[i + 1][k] != NULL)
-             if (grid[i + 1][k] == '#') counter++;
-           if (grid[i - 1][k] != NULL)
-             if (grid[i - 1][k] == '#') counter++;
-           if (grid[i][k + 1] != NULL)
-             if (grid[i][k + 1] == '#') counter++;
-           if (grid[i][k - 1] != NULL)
-             if (grid[i][k - 1] == '#') counter++;
-
-           if (counter == 3) grid[i][k] = '#';
+           else if(counter == 3)
+           {
+             temp[i][k] = '#';
+           }
+           else if(counter > 3)
+           {
+             temp[i][k] = '.';
+           }
+           }
+         else if(grid[i][k] == '.')
+         {
+           if (counter == 3) temp[i][k] = '#';
          }
        }
+     }
+
+     for (int t1 = 0; t1 < C; t1++)
+     {
+       for(int t2 = 0; t2<L; t2++)
+     {
+       grid[t1][t2] = temp[t1][t2];
+       temp[t1][t2] = '.';
+     }
      }
    }
 

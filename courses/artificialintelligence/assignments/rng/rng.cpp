ab5dc8fe78a4136a374649fda43fd3a85832d911
yendo19 <90066698+yendo19@users.noreply.github.com>
2023-09-21 14:14:15 -0400

    Update rng.cpp

--- courses/artificialintelligence/assignments/rng/rng.cpp
+++ courses/artificialintelligence/assignments/rng/rng.cpp
@@ -1,4 +1,19 @@
 // add your imports here
+#include <iostream>
+#include <stdint.h>
+using namespace std;
 int main(){
   // code here
+  int seed, number;
+  int r1, r2;
+  cin >> seed >>  number >>  r1 >> r2;
+
+  for(int i = 0; i<number; i++)
+  {
+    seed ^= seed << 13;
+    seed ^= seed >> 17;
+    seed ^= seed << 5;
+    seed  = r1 + (abs(seed) % (r2 - r1 + 1));
+    cout << seed << "\n";
+  }
 }

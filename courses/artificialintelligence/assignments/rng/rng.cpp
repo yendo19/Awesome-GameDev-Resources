5ec70490270ec091c57d9950e27e50aafc8315f6
Yuuki Endo <yuuki.endo@mymail.champlain.edu>
2023-09-24 20:57:23 -0400

    Update rng.cpp

--- courses/artificialintelligence/assignments/rng/rng.cpp
+++ courses/artificialintelligence/assignments/rng/rng.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 int main(){
   // code here
-  int seed, number;
-  int r1, r2;
+  uint32_t seed, number;
+  int32_t r1, r2;
   cin >> seed >>  number >>  r1 >> r2;
 
   for(int i = 0; i<number; i++)
@@ -13,7 +13,6 @@ int main(){
     seed ^= seed << 13;
     seed ^= seed >> 17;
     seed ^= seed << 5;
-    seed  = r1 + (abs(seed) % (r2 - r1 + 1));
-    cout << seed << "\n";
+    cout << r1 + seed % (r2-r1+1) << endl;
   }
 }

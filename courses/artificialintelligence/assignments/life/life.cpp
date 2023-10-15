d6aa4357891d5135fda97660f70bb600755b72b9
Yuuki Endo <yuuki.endo@mymail.champlain.edu>
2023-10-15 03:06:32 -0400

    diff implemtation

--- courses/artificialintelligence/assignments/life/life.cpp
+++ courses/artificialintelligence/assignments/life/life.cpp
@@ -1,86 +1,78 @@
 #include <iostream>
+#include <list>
 using namespace std;
-int main(){
-  //stuff
-    int C, L, T;
-    int counter;
+int getNeighbors(int x, int y, int c, int l, char**arr );
+int main() {
+  // stuff
+  int counter;
+  int C, L, T;
+  // inputs
+  cin >> L >> C >> T;
 
-    //inputs
-   cin >> L >> C >> T;
+  // grids
+  char grid[C][L];
+  char temp[C][L];
 
-   //grids
-   char grid[C][L];
-   char temp[C][L];
+  // get input
+  for (int i = 0; i < C; i++) {
+    for (int k = 0; k < L; k++) {
+      cin >> grid[i][k];
+      temp[i][k] = '.';
+    }
+  }
+  int count = 0;
+  int iterations = 8;
+              //left   middle   right
+  int xDiff[] = {-1,-1,-1,0,0,1,1,1};
+  int yDiff[] = {-1,0,1,-1,1,-1,0,1};
+  // rounds       //b,m,t               b, m        b,m,t
+  for (int j = 0; j < T; j++) {
+    // columns
+    for (int x = 0; x < C; x++) {
+      // rows
+      for (int y = 0; y < L; y++) {
 
-   //get input
-   for(int i = 0; i<C; i++)
-   {
-     for(int k = 0; k<L; k++)
-     {
-       cin >> grid[i][k];
-       temp[i][k] = '.';
-     }
-   }
-   //rounds
-   for(int j = 0; j<T; j++) {
-     //columns
+        for(int i = 0; i < iterations; i++)
+        {
+          int tempX = (x + xDiff[i] + C) % C;
+          int tempY = (y + yDiff[i] + L) % L;
+
+          if(grid[tempX][tempY] == '#')
+            count++;
+
+        }
+
+
+        if (grid[x][y] == '#')
+          if (count < 2 || count > 3)
+            temp[x][y] = '.';
+          else
+            temp[x][y] = '#';
+
+        else if (grid[x][y] == '.')
+          if(count == 3)
+            temp[x][y] = '#';
+          else
+            temp[x][y] = '.';
+
+        count = 0;
+      }
+    }
+    for (int t1 = 0; t1 < C; t1++) {
+      for (int t2 = 0; t2 < L; t2++) {
+        grid[t1][t2] = temp[t1][t2];
+      }
+    }
+  }
+
+
+
+     // write out grid
      for (int i = 0; i < C; i++) {
-       //rows
        for (int k = 0; k < L; k++) {
-         counter = 0;
-
-         /*
-          *    U
-          * L  *  R
-          *    D
-          */
-         for(int c1 = -1; c1 <= 1; c1++) {
-           for (int c2 = -1; c2 <= 1; c2++) {
-             if (c1 == 0 && c2 == 0) continue;
-             int temp1 = (i + c1 + C) % C;
-             int temp2 = (k + c2 + L) % L;
-
-             if (grid[temp1][temp2] == '#') counter++;
-           }
-         }
-         //Living/Dying logic
-         if (grid[i][k] == '#')
-         {
-           if(counter < 2 || counter >3)
-           {
-             temp[i][k] = '.';
-           }
-           else {
-             temp[i][k] = '#';
-           }
-           }
-         else
-         {
-           if (counter == 3) temp[i][k] = '#';
-           else
-             temp[i][k] = '.';
-         }
+         cout << grid[i][k];
        }
+       cout << endl;
      }
-
-
-     // replace old grid with temp grid
-     for (int t1 = 0; t1 < C; t1++)
-     {
-       for(int t2 = 0; t2<L; t2++)
-     {
-       grid[t1][t2] = temp[t1][t2];
-     }
-     }
-   }
-
-   //write out grid
-   for(int i = 0; i<C; i++)
-   {
-     for(int k = 0; k<L; k++)
-     {
-       cout << grid[i][k];
-     }
-     cout << endl;
-   }
 };
+
